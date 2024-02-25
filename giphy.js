d860886bdb8e9629d1730d6c8789d5e7e482e988
Zsolt Nagy <info@zsoltnagy.eu>
2024-02-25 15:43:30 +0100

    displaying slider value

--- giphy.js
+++ giphy.js
@@ -1,1 +1,5 @@
+function updateSliderValue(event) {
+    document.querySelector(".js-gif-range-value").innerText = event.target.value;
+}
 
+document.querySelector("[name=gif-range]").addEventListener("input", updateSliderValue);
